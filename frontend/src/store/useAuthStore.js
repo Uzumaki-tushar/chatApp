18f65d1d79628f75871239b2c9213b9f58f92418
Uzumaki-tushar <tusharsourav01@gamil.com>
2025-04-04 02:11:26 +0530

    created profile page and login logut and update profile feature

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,13 +1,14 @@
 import {create} from "zustand";
 import { axiosInstance } from "../lib/axios";
 import {toast} from "react-hot-toast";
+import { data } from "react-router-dom";
 
 
 export const useAuthStore= create((set)=>({
     authUser:null,
     isSigningUp: false,
     isLoggingIn:false,
-    isUpdatingProfile: false,
+    isUpdatingProfile:false,
     isCheckingAuth:true,
 
     checkAuth: async()=>{
@@ -61,6 +62,22 @@ export const useAuthStore= create((set)=>({
         }
         finally{
             set({isLoggingIn:false});
+        }
+    },
+
+    updateProfile: async(data)=>{
+        set({isUpdatingProfile:true});
+        try{
+            const res= await axiosInstance.put("/auth/update-profile",data);
+            set({authUser:res.data});
+            toast.success("Profile updated successfully");
+        }
+        catch(error){
+            console.log("error in update profile:",error);
+            toast.error(error.response.data.message);
+        }
+        finally{
+            set({isUpdatingProfile:false});
         }
     }
 
