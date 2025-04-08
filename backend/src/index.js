9c83b4578175c5beb374a5545cc5e73d0fcfcf3a
Uzumaki-tushar <tusharsourav01@gamil.com>
2025-04-09 01:00:54 +0530

    new commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -30,12 +30,17 @@ app.use(cors(
 app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 
-if(process.env.NODE_ENV==='kproduction'){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+if(process.env.NODE_ENV==='production'){
+    // app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-    })
+    // app.get("*",(req,res)=>{
+    //     res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    // })
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+      });
 }
 
 server.listen(PORT,()=>{
